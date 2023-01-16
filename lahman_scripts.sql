3ec8d6de486c497906a4389c7f4baa067b0ab921
aaustin07 <aaustin07@outlook.com>
2023-01-16 16:13:19 -0600

    3 is done

--- lahman_scripts.sql
+++ lahman_scripts.sql
@@ -16,20 +16,90 @@
 --Paet C. What team did he play for --what tbl
        --teams tbl -- I need columns 'name'(full team name), 'G'(games played)
 --Part A
-SELECT
-p.namegiven AS full_name,
-t.name AS team_name,
---a.teamid,
-a.g_all AS total_games,
-MIN(p.height) AS shortest_height
+-- SELECT
+-- p.namegiven AS full_name,
+-- t.name AS team_name,
+-- --a.teamid,
+-- a.g_all AS total_games,
+-- MIN(p.height) AS shortest_height
+
+-- FROM people p
+-- LEFT JOIN appearances a 
+-- ON p.playerid = a.playerid
+-- LEFT JOIN teams t
+-- ON a.teamid = t.teamid
+
+-- GROUP BY 1,2,3
+-- ORDER BY shortest_height asc
+-- LIMIT 10
+--Edward Carl at 43 inches played 1 game.
+
+-- 3.Find all players in the database who played at Vanderbilt University. 
+-- Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. 
+-- Sort this list in descending order by the total salary earned. 
+-- Which Vanderbilt player earned the most money in the majors?
+
+--A. Find all players that played at Vanderbilt? Need tbls--collegeplaying, schools, people 
+
+-- SELECT DISTINCT
+-- p.namefirst AS first_name,
+-- p.namelast AS last_name,
+-- s.schoolname
+
+-- FROM people p
+-- LEFT JOIN collegeplaying cp
+-- ON p.playerid = cp.playerid
+-- LEFT JOIN schools s
+-- ON cp.schoolid = s.schoolid
+
+-- WHERE 
+-- s.schoolname = 'Vanderbilt University'
+-- --24 Vanderbilt University Players.
+
+--B. Find tbl that contains salary earned in major leagues --tbl salaries & appearances to get the
+--let's look at salaries, people and collegeplaying tbls
+-- SELECT 
+-- s.playerid,
+-- MONEY(CAST(SUM(s.salary)AS Numeric)) AS total_sal
+
+-- FROM salaries s
+-- LEFT JOIN collegeplaying cp
+-- ON s.playerid = cp.playerid
+
+-- WHERE 
+-- cp.schoolid = 'vandy'
+-- GROUP BY 1
+-- ORDER BY total_sal DESC
+--15 players from vandy appeared to have earned a salary from the majors.
+
+--C. Lets put it all together
+
+SELECT DISTINCT
+p.namefirst AS first_name,
+p.namelast AS last_name,
+b.total_sal
 
 FROM people p
-LEFT JOIN appearances a 
-ON p.playerid = a.playerid
-LEFT JOIN teams t
-ON a.teamid = t.teamid
+LEFT JOIN collegeplaying cp
+ON p.playerid = cp.playerid
 
-GROUP BY 1,2,3
-ORDER BY shortest_height asc
-LIMIT 10
---Edward Carl at 43 inches played 1 game.
+INNER JOIN --changed from LEFT JOIN to INNER becuase the total_sal column was null. When changed to INNER I got my results
+(
+SELECT 
+s.playerid,
+MONEY(CAST(SUM(s.salary)AS Numeric)) AS total_sal
+
+FROM salaries s
+LEFT JOIN collegeplaying cp
+ON s.playerid = cp.playerid
+
+WHERE 
+cp.schoolid = 'vandy'
+GROUP BY 1    
+) b
+ON p.playerid = b.playerid
+
+ORDER BY b.total_sal DESC
+--15 players from Vanderbilt 
+
+
